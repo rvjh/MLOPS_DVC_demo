2da0482937caea3aa456ade9148c4f32e08a7188
Rohan Das <mailtorohan.97@gmail.com>
2021-10-07 01:28:11 +0530

    stage 1 complete

---
+++ src/load_data.py
@@ -0,0 +1,26 @@
+# read the data from data_given
+# save it in data/raw for further process
+
+import os
+from get_data import read_params, get_data
+import argparse
+
+
+def load_and_save(config_path):
+    config = read_params(config_path)
+    df = get_data(config_path)
+    """
+    replacing space with "_"
+    """
+    new_cols = [col.replace(" ","_") for col in df.columns]
+    #print(new_cols)
+    raw_data_path = config["load_data"]["raw_dataset_csv"]
+    df.to_csv(raw_data_path,sep=",",index=False,header=new_cols)
+
+
+
+if __name__=="__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", default="params.yaml")
+    parsed_args = args.parse_args()
+    load_and_save(config_path=parsed_args.config)
